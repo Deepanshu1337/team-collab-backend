7403cb7fa072892cafbaa822ca8622384f341d6a
Deepanshu Dixit <deepanshudixit07@gmail.com>
2026-01-05 08:32:38 +0530

    added

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,13 +6,10 @@ import env from "./env.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Path to service account key
-const serviceAccountPath = path.join(__dirname, String(env.firebaseServiceAccountPath));
-
 // Initialize Firebase Admin only once
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountPath),
+    credential: admin.credential.cert(process.env.firebase-service-account.json),
   });
 }
 
